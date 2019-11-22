1d9bfeff6e9b7086271f1c86663542b80a3ce7ca
Tor Shepherd <tshephe2@illinois.edu>
2019-11-22 14:47:42 -0500

    added decode and encode functions

---
+++ decodeColocationVector.m
@@ -0,0 +1,15 @@
+function [x, u, y, v, psi, r, delta, Fx] = decodeColocationVector(z)
+    % 8n + 6 = size(z,1)
+    n = (size(z, 1) - 6) / 8;
+    l1 = (n+1)*6;
+    l2 = n*2;
+    z = reshape(z, [l1+l2,1]);
+    x = z(1:6:l1);
+    u = z(2:6:l1);
+    y = z(3:6:l1);
+    v = z(4:6:l1);
+    psi = z(5:6:l1);
+    r = z(6:6:l1);
+    delta = z(l1+1:2:l1+l2);
+    Fx = z(l1+2:2:l1+l2);
+end
---
+++ plotPart1.m
@@ -0,0 +1,13 @@
+generatePart1
+
+load TestTrack.mat
+
+sol1 = forwardIntegrateControlInput(ROB535ControlsProjectpart1input);
+
+plot(TestTrack.cline(1,:), TestTrack.cline(2,:), '--k')
+hold on
+plot(TestTrack.bl(1,:), TestTrack.bl(2,:), 'k')
+plot(TestTrack.br(1,:), TestTrack.br(2,:), 'k')
+plot(sol1(:,1), sol1(:,3), 'b')
+%xlim([200,1500])
+%ylim([-200, 825])
