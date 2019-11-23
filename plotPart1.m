2e208fec5aee365a74bf0d1a5a710aef3dea38ee
Tor Shepherd <tshephe2@illinois.edu>
2019-11-22 21:16:11 -0500

    changed plotting slightly

--- plotPart1.m
+++ plotPart1.m
@@ -6,8 +6,8 @@ sol1 = forwardIntegrateControlInput(ROB535ControlsProjectpart1input);
 
 plot(TestTrack.cline(1,:), TestTrack.cline(2,:), '--k')
 hold on
-plot(TestTrack.bl(1,:), TestTrack.bl(2,:), 'k')
-plot(TestTrack.br(1,:), TestTrack.br(2,:), 'k')
+plot(TestTrack.bl(1,:), TestTrack.bl(2,:), 'ok')
+plot(TestTrack.br(1,:), TestTrack.br(2,:), 'ok')
 plot(sol1(:,1), sol1(:,3), 'b')
 %xlim([200,1500])
 %ylim([-200, 825])
