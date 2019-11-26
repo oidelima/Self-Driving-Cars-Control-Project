4f19cf19530e38ecfd390f644f3ca9dcc6caf08c
Brian Fogelson <bfogels1@jhu.edu>
2019-11-26 14:57:15 -0500

    Modified loss

--- lossfun.m
+++ lossfun.m
@@ -1,16 +1,18 @@
-function [L, dL_dU] = lossfun(curr_state, curr_input, TestTrack) %, goal_x, goal_y)
+function [L, dL_dU] = lossfun(curr_state, curr_input, TestTrack, cline_interp) %, goal_x, goal_y)
     x = curr_state(1);
     y = curr_state(3);
 
-    L = hill(x,y, TestTrack);
+    %L = hill(x,y, TestTrack);
+    L = hill2(x,y, TestTrack, cline_interp);
     
     hill_step = .05;
-    dL_dxy = gradientHill(x,y,TestTrack,hill_step);
+    dL_dxy = gradientHill(x,y,TestTrack,cline_interp, hill_step);
 
     % Step 1 forward integrate curr_input to get numerical gradient of
     % dx/dU and dy/dU with central difference
-    step_size = .001;
-    num_simulated = 5; % >= 3 or the program crashes in ode45
+    step_size_u1 = .05; % delta goes from -.5 to .5
+    step_size_u2 = 50; % Fx goes from -5000 to 5000
+    num_simulated = 3; % 5 % >= 3 or the program crashes in ode45
     u1 = curr_input(1);
     u2 = curr_input(2);
     
@@ -18,12 +20,12 @@ function [L, dL_dU] = lossfun(curr_state, curr_input, TestTrack) %, goal_x, goal
     [output_0,~] = forwardIntegrateControlInput([u1*ones(num_simulated,1), u2*ones(num_simulated,1)], curr_state);
     
     % Shift u1 by step size and forward integrate
-    [output_u1p1,~] = forwardIntegrateControlInput([(u1+step_size)*ones(num_simulated,1), u2*ones(num_simulated,1)], curr_state);
-    [output_u1m1,~] = forwardIntegrateControlInput([(u1-step_size)*ones(num_simulated,1), u2*ones(num_simulated,1)], curr_state);
+    [output_u1p1,~] = forwardIntegrateControlInput([(u1+step_size_u1)*ones(num_simulated,1), u2*ones(num_simulated,1)], curr_state);
+    [output_u1m1,~] = forwardIntegrateControlInput([(u1-step_size_u1)*ones(num_simulated,1), u2*ones(num_simulated,1)], curr_state);
     
     % Shift u2 by step size and forward integrate
-    [output_u2p1,~] = forwardIntegrateControlInput([u1*ones(num_simulated,1), (u2+step_size)*ones(num_simulated,1)], curr_state);
-    [output_u2m1,~] = forwardIntegrateControlInput([u1*ones(num_simulated,1), (u2-step_size)*ones(num_simulated,1)], curr_state);
+    [output_u2p1,~] = forwardIntegrateControlInput([u1*ones(num_simulated,1), (u2+step_size_u2)*ones(num_simulated,1)], curr_state);
+    [output_u2m1,~] = forwardIntegrateControlInput([u1*ones(num_simulated,1), (u2-step_size_u2)*ones(num_simulated,1)], curr_state);
     
     % Assign x's and y's from the last step of the simulation
     x_0 = output_0(end,1);
@@ -38,23 +40,23 @@ function [L, dL_dU] = lossfun(curr_state, curr_input, TestTrack) %, goal_x, goal
     y_u2m1 = output_u2m1(end,3); 
     
     % dx_du1
-    cd1_dx_du1 = (x_u1p1 - x_0) / step_size;
-    cd2_dx_du1 = (x_0 - x_u1m1) / step_size;
+    cd1_dx_du1 = (x_u1p1 - x_0) / step_size_u1;
+    cd2_dx_du1 = (x_0 - x_u1m1) / step_size_u1;
     dx_du1 = (cd1_dx_du1 + cd2_dx_du1) / 2;
     
     % dx_du2
-    cd1_dx_du2 = (x_u2p1 - x_0) / step_size;
-    cd2_dx_du2 = (x_0 - x_u2m1) / step_size;
+    cd1_dx_du2 = (x_u2p1 - x_0) / step_size_u2;
+    cd2_dx_du2 = (x_0 - x_u2m1) / step_size_u2;
     dx_du2 = (cd1_dx_du2 + cd2_dx_du2) / 2;
     
     % dy_du1
-    cd1_dy_du1 = (y_u1p1 - y_0) / step_size;
-    cd2_dy_du1 = (y_0 - y_u1m1) / step_size;
+    cd1_dy_du1 = (y_u1p1 - y_0) / step_size_u1;
+    cd2_dy_du1 = (y_0 - y_u1m1) / step_size_u1;
     dy_du1 = (cd1_dy_du1 + cd2_dy_du1) / 2;
     
     % dy_du2
-    cd1_dy_du2 = (y_u2p1 - y_0) / step_size;
-    cd2_dy_du2 = (y_0 - y_u2m1) / step_size;
+    cd1_dy_du2 = (y_u2p1 - y_0) / step_size_u2;
+    cd2_dy_du2 = (y_0 - y_u2m1) / step_size_u2;
     dy_du2 = (cd1_dy_du2 + cd2_dy_du2) / 2;
     
     
@@ -65,21 +67,52 @@ function [L, dL_dU] = lossfun(curr_state, curr_input, TestTrack) %, goal_x, goal
 
 end
 
-% function g = inequalityConstraintTrack(pos, leftBound, rightBound)
-%     [leftNormals, rightNormals] = calcNormals(leftBound, rightBound);
-%     Idleft = knnsearch(leftBound', pos');
-% 	Idright = knnsearch(rightBound', pos');
-% 
-%     rightCorr = rightBound(:, Idright');
-%     leftCorr = leftBound(:, Idleft');
-% 
-%     rightVectors = pos - rightCorr;
-%     leftVectors = pos - leftCorr;
-%     rightNormalsCorr = rightNormals(:, Idright');
-%     leftNormalsCorr = leftNormals(:, Idleft');
-% 
-%     g = -(dot(rightVectors, rightNormalsCorr, 1) .* dot(leftVectors, leftNormalsCorr, 1));
-% end
+function z = hill2(x,y, TestTrack, cline_interp)
+    pos = [x;y];
+    leftBound = TestTrack.bl;
+    rightBound = TestTrack.br;
+    [leftNormals, rightNormals] = calcNormals(leftBound, rightBound);
+    Idleft = knnsearch(leftBound', pos');
+	Idright = knnsearch(rightBound', pos');
+
+    rightCorr = rightBound(:, Idright');
+    leftCorr = leftBound(:, Idleft');
+
+    rightVectors = pos - rightCorr;
+    leftVectors = pos - leftCorr;
+    rightNormalsCorr = rightNormals(:, Idright');
+    leftNormalsCorr = leftNormals(:, Idleft');
+
+    z1 = -(dot(rightVectors, rightNormalsCorr, 1) .* dot(leftVectors, leftNormalsCorr, 1)); % * 10^6;
+    
+    % N = size(TestTrack.cline, 2);
+    N = size(cline_interp, 2);
+    %xy_rep = repmat([x;y], 1, N);
+%     [~,left_bound_ind] = min(norm(TestTrack.bl-xy_rep));
+%     left_bound = TestTrack.bl(left_bound_ind);
+%     [~,right_bound_ind] = min(norm(TestTrack.br-xy_rep));
+%     right_bound = TestTrack.br(right_bound_ind);
+    diff_norms = zeros(1,N);
+    for i = 1:N
+        diff_norms(i) = norm(cline_interp(:,i)-[x;y]);
+    end
+    [~,cline_ind] = min(diff_norms);
+%     cpoint = TestTrack.cline(cline_ind+1);
+%     next_cpoint = TestTrack.cline(cline_ind + 2);
+%     next_next_cpoint = TestTrack.cline(cline_ind + 3);
+
+     z2 = N - cline_ind;  %;
+     progress_bar = 100*(cline_ind / N)
+     
+     z3 = norm(cline_interp(:,cline_ind+100) - [x;y] );
+    
+    % z2 = 2*norm([x;y] - cpoint)^2 + 5*norm([x;y] - next_cpoint)^2 + 5*norm([x;y] - next_next_cpoint)^2;
+    % z2 = sqrt(z2)/10;% /10;
+
+    % z = z1 + z2;
+    z = 10000*z1 + z2 + z3*10000; % was times
+    z = 1000000*z1 + z2 + z3*100000; % was times
+end
 
 function [Lnormals, Rnormals] = calcNormals(leftBound, rightBound)
     n = size(leftBound, 2); % 246
@@ -91,33 +124,34 @@ function [Lnormals, Rnormals] = calcNormals(leftBound, rightBound)
     Rnormals(:,n) = Rnormals(:,n - 1);
 end
 
-function z = hill(x,y, TestTrack)
-    % Can pass in interpolated TestTrack
-    N = size(TestTrack, 2);
-    xy_rep = repmat([x;y], 1, N);
-    [~,left_bound_ind] = min(norm(TestTrack.bl-xy_rep));
-    left_bound = TestTrack.bl(left_bound_ind);
-    [~,right_bound_ind] = min(norm(TestTrack.br-xy_rep));
-    right_bound = TestTrack.br(right_bound_ind);
-    [~,cline_ind] = min(norm(TestTrack.cline-xy_rep));
-    cpoint = TestTrack.cline(cline_ind);
-    
-    next_cpoint = TestTrack.cline(cline_ind + 1);
-    next_next_cpoint = TestTrack.cline(cline_ind + 2);
-    
-    z = 2*norm([x;y] - cpoint)^2 + 5*norm([x;y] - next_cpoint)^2 + norm([x;y] - next_next_cpoint)^2;
-    
-    if norm([x;y] - cpoint) > norm(cpoint - left_bound)
-        z = z + 10000;
-    elseif norm([x;y] - cpoint) > norm(cpoint - right_bound)
-       z = z + 10000;     
-    end
-    
-    %z = inequalityConstraintTrack([x;y], left_bound, right_bound)
-end
+% function z = hill(x,y, TestTrack)
+%     % Can pass in interpolated TestTrack
+%     N = size(TestTrack, 2);
+%     xy_rep = repmat([x;y], 1, N);
+%     [~,left_bound_ind] = min(norm(TestTrack.bl-xy_rep));
+%     left_bound = TestTrack.bl(left_bound_ind);
+%     [~,right_bound_ind] = min(norm(TestTrack.br-xy_rep));
+%     right_bound = TestTrack.br(right_bound_ind);
+%     [~,cline_ind] = min(norm(TestTrack.cline-xy_rep));
+%     cpoint = TestTrack.cline(cline_ind);
+%     
+%     next_cpoint = TestTrack.cline(cline_ind + 1);
+%     next_next_cpoint = TestTrack.cline(cline_ind + 2);
+%     
+%     z = 2*norm([x;y] - cpoint)^2 + 5*norm([x;y] - next_cpoint)^2 + norm([x;y] - next_next_cpoint)^2;
+%     
+%     if norm([x;y] - cpoint) > norm(cpoint - left_bound)
+%         z = z + 100000;
+%     elseif norm([x;y] - cpoint) > norm(cpoint - right_bound)
+%        z = z + 100000;     
+%     end
+%     
+%     %z = inequalityConstraintTrack([x;y], left_bound, right_bound)
+% end
 
-function dg = gradientHill(x,y,TestTrack, step_size)
-    f = @(x,y) hill(x,y,TestTrack) ;
+function dg = gradientHill(x,y,TestTrack,cline_interp,step_size)
+    %f = @(x,y) hill(x,y,TestTrack) ;
+    f = @(x,y) hill2(x,y,TestTrack,cline_interp) ;
     [df_dx, df_dy] = brianNumGradient(f,x,y,step_size);
     dg = [df_dx df_dy];
 end
--- test_entire_trajectory.m
+++ test_entire_trajectory.m
@@ -1,13 +1,15 @@
 clear
 load TestTrack.mat
+num_passed = 0
 
 % Interpolate Test Track?
-% num_interpl = 5;
-% cline_interp = interp_cline(TestTrack.cline, num_interpl);
+num_interpl = 100; % 1000
+cline_interp = interp_cline(TestTrack.cline, num_interpl);
 % plot(TestTrack.cline(1,:), TestTrack.cline(2,:) )
 % hold on
 % plot(cline_interp(1,:), cline_interp(2,:) )
 
+%%
 start_pos = [287,5,-176,0,2,0];
 start_input = [0.01, 0.01];
 
@@ -21,17 +23,13 @@ U(1,:) = start_input;
 i = 1;
 %for i = 1:N:size(cline_interp,2)-N+1
 while ~close_to_end(Y(i,:),TestTrack)
-    % Start pos
+    % Curr pos
     curr_state = Y(i,:);
     curr_input = U(i,:);
     
-%     % Estimate the test_track
-%     %track_section = TestTrack.cline(:,i:i+5);
-%     track_section = cline_interp(:,i:i+N-1);
-    
     % Generate N inputs based on current position
     N = 3;
-    U_temp = getControlInput(curr_state, curr_input,TestTrack, N);
+    U_temp = getControlInput(curr_state, curr_input,TestTrack, cline_interp, N);
     
     %Simulate forwards to get next state from U
     [Y_temp,T]=forwardIntegrateControlInput(U_temp,curr_state);
@@ -40,42 +38,62 @@ while ~close_to_end(Y(i,:),TestTrack)
     Y(i+1:i+N,:) = Y_temp;
     U(i+1:i+N,:) = U_temp;
     i = i + N;
+    
+    if num_passed > 2
+        close all;
+        plot(Y(:,1),Y(:,3))
+        hold on
+        plot(TestTrack.cline(1,:),TestTrack.cline(2,:))
+        plot(TestTrack.br(1,:),TestTrack.br(2,:))
+        plot(TestTrack.bl(1,:),TestTrack.bl(2,:))
+        ylim([-200,800])
+        xlim([200,1500])
+        num_passed = 0;
+    end
+    num_passed = num_passed + 1;
+    
 end
 
-function U = getControlInput(curr_state, curr_input,TestTrack,N)
+function U = getControlInput(curr_state, curr_input,TestTrack,cline_interp,N)
     % Calculate loss and derivative of loss with respect to input
-    [L, dL_dU] = lossfun(curr_state, curr_input,TestTrack);
+    [L, dL_dU] = lossfun(curr_state, curr_input,TestTrack,cline_interp);
     L;
     
     % Learning rate. Can tune
-    eta = .5; % .5 gets closer to end % 300 still slowly gets better
+    eta = .3; % 10; % 1.8; % .5 gets closer to end % 300 still slowly gets better
     
-    % Gradient Descent N steps
+    % Gradient Descent N steps, all the same
     U = ones(N,1) * (curr_input - eta * dL_dU);
+    
+    % Simulate N times in that direction
 %     U(1,:) = (curr_input - eta * dL_dU);
 %     for i = 1:N-1
 %         U(i+1,:) = U(i,:) - eta * dL_dU;
 %     end
+
 end
 
 function is_close = close_to_end(curr_state,TestTrack)
     x = curr_state(1);
     y = curr_state(3);
     last_point = TestTrack.cline(:,end);
-    dist = norm(last_point-[x;y])
+    dist = norm(last_point-[x;y]);
     is_close = ( dist < 20 );
 end
 
 
-% function cline_interp = interp_cline(cline, num_interpl)
-%     num_points = size(cline,2);
-%     
-%     for i = 1:num_points
-%         x = cline(1,i);
-%         y = cline(2,i);
-%     end
-%     
-%     cline_interp = cline;
-% end
+function cline_interp = interp_cline(cline, num_interpl)
+    N = size(cline,2);
+    step_size = 1/num_interpl;
+    
+    x_points = cline(1,:);
+    y_points = cline(2,:);
+    new_ind = 1:step_size:N;
+    
+    x_interp = interp1(x_points, new_ind);
+    y_interp = interp1(y_points, new_ind);
+    
+    cline_interp = [x_interp;y_interp];
+end
 
 
