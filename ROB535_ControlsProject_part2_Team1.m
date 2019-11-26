fd83184852287e5782242df2dc070c55f2fda1dd
yswi <yswi@umich.edu>
2019-11-26 12:15:10 -0500

    almost

--- ROB535_ControlsProject_part2_Team1.m
+++ ROB535_ControlsProject_part2_Team1.m
@@ -1,6 +1,12 @@
 %function [sol_2, FLAG_terminate] = ROB535_ControlsProject_part2_Team1 (TestTrack,Xobs_seen,curr_state)
 
-%%% where sol 1 is the vehicle?s trajectory.
+
+if curr_state == 
+    FLAG_terminate = 1
+else
+    FLAG_terminate = 0
+end
+
 
 %% sol 2 is a vector of control inputs that will be passed to forwardIntegrateControlInput. 
 %As mentioned later in 6.2, sol 2 must have enough control inputs with time step 0.01 sec- onds to forward integrate vehicle dynamics for the next 0.5 second.
@@ -30,7 +36,6 @@ syms x1 x2 x3 x4 x5 x6 u1 u2
 a_f=rad2deg(u1-atan2(x4+a*x6,x2));
 a_r=rad2deg(-atan2((x4-b*x6),x2));
  
- 
 F_zf=b/(a+b)*m*g;
 F_yf=F_zf*Dy*sin(Cy*atan(By*((1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy)))))+Svy;
  
@@ -53,36 +58,32 @@ B = jacobian(dzdx,[u1;u2]);
 % horizon of 0.5 s & 0.01 s = 50 timestep
 Ndec = 6*51 + 2*5;
 npred = 50;
+xsize = 6*51;
+zsize = Ndec;
 
 % Staates : z  &  Input : u 
 %z = zeros(6*51 + 2*5, 1 );
-%dzdt=bike(t,x,T,U)
 
-%% Equality Constraints
-% quadprog
-% Euler integration  
-xsize = 6*51;
-zsize = Ndec;
-Aeq=zeros(xsize,zsize);
-Beq=zeros(xsize,1);
 
+
+% State Constraints 
+
+[leftlimit, rightlimit] = bound(npred,states,leftbound, rightbound);
+state_lb = -inf(Ndec,1);
+state_ub = inf(Ndec,1);
 
 for i = 1 : npred
-    Am = A(i);
-    Bm = B(i);
+    state_lb(6* (i-1) +1) =  min(leftlimit(1,i), rightlimit(1,i));
+    state_lb(6* (i-1) +3) =  min(leftlimit(2,i), rightlimit(2,i));
     
-    Aeq(6*(i)+1 :6*(i)+6 , 6*(i-1)+1:6*(i-1)+6) = Am ;
-    Aeq(6*(i)+1:6*(i)+6, 6*(i-1)+1+6) = -ones(6,1);  
-    
-    Aeq(6*(i)+1:6*(i)+6 ,xsize+2*(i-1)+1:xsize+2*(i-1)+2) = Bm;
+    state_ub(6* (i-1) +1) = max(leftlimit(1,i), rightlimit(1,i));
+    state_ub(6* (i-1) +3) = max(leftlimit(2,i), rightlimit(2,i));
 end
 
-Aeq(1:6,1:6) = eye(6);
-Beq(1:6) = reshape(curr_state,6,1);
 
 
-%%Boundary Constraints
-% 4-by-2 matrix 
+
+% Input constraints 
     Lb = -inf(Ndec,1);
     Ub = inf(Ndec,1);
     j=0;
@@ -97,7 +98,18 @@ Beq(1:6) = reshape(curr_state,6,1);
 
 
 %% quadatic penalty
+% !! have to call the result of part 1 for calling Y_ref / U_ref !! 
+    STATE_tilde = urr_state - Y_ref;
+    [Aeq,beq] = eq_cons(npred, xsize, zsize, A,B, STATE_tilde);
+    [Lb,Ub] = bound_cons(U_ref);
+    
+    
+    sol = quadprog(H,zeros(53,1),[],[],Aeq,beq,Lb,Ub);
 
+
+    %extract control input from solution   
+    %forward integrate using nonlinear model, STATE(i), the extracted control input and ode45
+    uin=sol(xsize+1 : xsize+2,1) + U_ref;
 
 
 
@@ -107,9 +119,112 @@ Beq(1:6) = reshape(curr_state,6,1);
 
 
 
+function [Aeq,Beq] = eq_cons(npred, xsize, zsize, A,B, curr_state)
+
+Aeq=zeros(xsize,zsize);
+Beq=zeros(xsize,1);
+
+for i = 1 : npred
+    Am = A(i);
+    Bm = B(i);
+    
+    Aeq(6*(i)+1 :6*(i)+6 , 6*(i-1)+1:6*(i-1)+6) = Am ;
+    Aeq(6*(i)+1: 6*(i)+6, 6*(i)+1 :6*(i)+6 ) = Aeq(6*(i)+1: 6*(i)+6, 6*(i)+1 :6*(i)+6) -eye(6);  
+    
+    Aeq(6*(i)+1:6*(i)+6 ,xsize+2*(i-1)+1:xsize+2*(i-1)+2) = Bm;
+end
+
+Aeq(1:6,1:6) = eye(6);
+Beq(1:6) = reshape(curr_state,6,1);
+
+end
 
 
 
+function[leftlimit, rightlimit] = bound(npred,states,leftbound, rightbound)
+    
+    % Output : leftlimit and the right limit of the road for the current
+    % location
+     
+    xypos = zeros(npred+1,2);
+    leftlimit = -inf(size(states))
+    rightlimit = inf(size(states))
+    
+    
+    for i = 1 : npred+1
+        xypos(i,1) = states(6 * (i-1) +1);
+        xypos(i,2) = states(6 * (i-1) +3);
+        Idleft_1 = knnsearch(leftBound', xypos(i,:));
+        Idright_1 = knnsearch(rightBound', xypos(i,:));
+        
+        % find a left and right pair of points to do a linear interpolation
+        % in order to get the exact leftbound and rightbound of a given
+        % position. Linear interpolation is done by using a "dot product".
+        
+        if dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1 +1)-leftbound(:, Idleft_1)) >= 0 
+            leftlimit = leftbound(:,Idleft_1) + dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1 +1)-leftbound(:, Idleft_1));
+        else
+            leftlimit = leftbound(:,Idleft_1) + dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1-1)-leftbound(:, Idleft_1));
+        end
+        
+        if dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 +1)-xypos(i,:)) >= 0 
+            rightlimit = dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 +1)-xypos(i,:));
+        else
+            rightlimit = dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 -1)-xypos(i,:));
+        end
+    end 
+ 
+end
 
 
-%
+%function restricting ith x and y with 1st order function
+% for a and b 
+
+function[a, b] = first_ineq(zsize,npred,states,leftbound,rightbound)
+   
+    a = zeros(2*(npred+1), zsize);
+    b = zeros(2*(npred+1),1);
+
+     
+     for i = 1 : npred+1
+        xpos = states(6 * (i-1) +1);
+        ypos = states(6 * (i-1) +3);
+        Idleft_1 = knnsearch(leftBound', [xpos,ypos]');
+        Idright_1 = knnsearch(rightBound',[xpos,ypos]');
+        
+        left_1 = leftbound(:,Idleft_1);
+        right_1 = rightbound(:,Idright_1);
+        
+        if dot([xpos,ypos]' - left_1,leftbound(:, Idleft_1 +1)-left_1) >= 0 
+            left_2 = left_1  + dot([xpos,ypos]' - lleft_1 ,leftbound(:, Idleft_1 +1)-left_1 );
+        else
+            left_2 = left_1  + dot([xpos,ypos]' - left_1 ,leftbound(:, Idleft_1-1)-left_1 );
+        end
+        
+        if dot([xpos,ypos]' - left_1,rightbound(:,Idright_1 +1)-left_1) >= 0 
+            right_2 = right_1+ dot([xpos,ypos]' - right_1,rightbound(:,Idright_1 +1)-right_1);
+        else
+            right_2 = right_1+ dot([xpos,ypos]' - right_1,rightbound(:,Idright_1 -1)-right_1);
+        end
+        
+        left_slope = (left_1(1) - left_2(1)) / (left_1(2) - left_2(2));
+        right_slope = (right_1(1) - right_2(1)) / (right_1(2) - right_2(2));
+        
+        a(2*(i-1) +1, 6 * (i-1) +1) = - left_slope;
+        a(2*(i-1) +1, 6 * (i-1) +3) = 1;
+        a(2*(i-1) +2, 6 * (i-1) +1) = - left_slope;
+        a(2*(i-1) +1, 6 * (i-1) +3) = -1;
+        
+        b(2*(i-1) +1) = left_slope * (-left_1(1)) + left_1(2);
+        b(2*(i-1) +2) = right_slope * (left_1(1)) - left_1(2);
+        
+     end       
+ end
+     
+
+% avoiding obstacles by elongating(adding) some rows a and b constraints % 
+
+function avoiding_obstacles(Xobs_seen)
+    obs_num = length(Xobs_seen)
+
+end
--- inequalityConstraintTrack.m
+++ inequalityConstraintTrack.m
@@ -1,3 +1,9 @@
+dis = zeros(length(TestTrack.bl(1,:)),1);
+
+for i = 1: length(length(TestTrack.bl(1,:)))
+    dis(i) = sqrt((TestTrack.bl(1,i) - TestTrack.br(1,i))^2 + (TestTrack.bl(2,i) - TestTrack.br(2,i))^2);
+end
+
 function g = inequalityConstraintTrack(pos, leftBound, rightBound)
     [leftNormals, rightNormals] = calcNormals(leftBound, rightBound);
     Idleft = knnsearch(leftBound', pos');
@@ -14,12 +20,9 @@ function g = inequalityConstraintTrack(pos, leftBound, rightBound)
     g = -(dot(rightVectors, rightNormalsCorr, 1) .* dot(leftVectors, leftNormalsCorr, 1));
 end
 
-function [Lnormals, Rnormals] = calcNormals(leftBound, rightBound)
+function [Lnormals, Rnormals] = calcNormals(pos,TestTrack.bl, TestTrack.br)
     n = size(leftBound, 2); % 246
     dLeft = leftBound(:,2:n) - leftBound(:,1:n-1);
     dRight = rightBound(:,2:n) - rightBound(:,1:n-1);
-    Lnormals = [0, -1; 1, 0] * (dLeft ./ vecnorm(dLeft));
-    Lnormals(:,n) = Lnormals(:,n - 1);
-    Rnormals = [0, 1; -1, 0] * (dRight ./ vecnorm(dRight));
-    Rnormals(:,n) = Rnormals(:,n - 1);
+
 end
