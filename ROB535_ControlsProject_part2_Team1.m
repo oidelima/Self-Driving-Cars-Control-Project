d17a0dc99570c7182b06c8f6bd37f2eafe4b7cf6
yswi <yswi@umich.edu>
2019-12-10 13:39:04 -0500

    works for new test data

--- ROB535_ControlsProject_part2_Team1.m
+++ ROB535_ControlsProject_part2_Team1.m
@@ -1,16 +1,133 @@
-%function [sol_2, FLAG_terminate] = ROB535_ControlsProject_part2_Team1 (TestTrack,Xobs_seen,curr_state)
+function [sol_2, FLAG_terminate] = ROB535_ControlsProject_part2_Team1 (TestTrack,Xobs_seen,curr_state)
 
+    
+    closest_obstacle_center = get_closest_obstacle(curr_state,Xobs_seen);
+    
+    centerLine = get_lane_from_obstacle_center( closest_obstacle_center, TestTrack);
+    
+    
+    centerLineTheta = TestTrack.theta;
+    FLAG_terminate = 0;
+    
+    % Times in seconds
+    total_time = .51;  % was .7
+    dt = .01;
+    
+    gains = [5, 10000;
+        0,0;
+        0,0];
+    % speed_ext = [10,25]; % speeds were 15 70
+    speed_ext = [10, 10];
+    lookahead = 2;
+    centerline_strength = 0.1;
 
-if curr_state == 
-    FLAG_terminate = 1
-else
-    FLAG_terminate = 0
+    % Gains
+    kp1 = gains(1,1);
+    ki1 = gains(2,1);
+    kd1 = gains(3,1);
+    kp2 = gains(1,2);
+    ki2 = gains(2,2);
+    kd2 = gains(3,2);
+    
+    %speed = 6;
+    
+    % Get cnormals
+    n_elements = size(centerLine, 2);
+    projectors = (centerLine(:,2:n_elements) - centerLine(:,1:n_elements-1));
+    projectors = projectors ./ vecnorm(projectors);
+    Cnormals = [0, 1; -1, 0] * projectors;
+    Cnormals(:,n_elements) = Cnormals(:,n_elements - 1);
+    
+    % Sum Error
+    sum_error = zeros(1,6);
+    prev_error = zeros(1,6);
+    U = [0, 0];
+    %Y_checking = [];
+    simulation_divergence = zeros(1,6);
+    
+    % Cline Distances
+    cline_distances = vecnorm(centerLine(:, 2:end) - centerLine(:, 1:end-1));
+    cline_distances(:,n_elements) = cline_distances(:,n_elements - 1);
+    d_ext = [min(cline_distances), max(cline_distances)];
+    
+    % d_theta
+    d_theta = 1./ abs(centerLineTheta(:, 2:end) - centerLineTheta(:, 1:end-1));
+    d_theta(:,n_elements) = d_theta(:,n_elements - 1);
+    d_theta_ext = [min(d_theta), max(d_theta)];
+    
+    
+    % Number of time steps
+    nsteps = total_time/dt;
+    
+    % Initialize inputs
+    %U = zeros(nsteps,2);
+    
+    for i=1:nsteps
+        nearest_goal_id = knnsearch(centerLine', [curr_state(1), curr_state(3)]);
+        lookahead_goal_id = nearest_goal_id + lookahead;
+        if lookahead_goal_id > size(centerLine, 2)
+            lookahead_goal_id = size(centerLine, 2);
+        end
+        %disp("Tracking point (out of 246) and iteration: ");
+        %disp([nearest_goal_id, i]);
+        nearest_goal = centerLine(:, nearest_goal_id);
+        length_scale = (d_theta(:, nearest_goal_id) - d_theta_ext(1))/(d_theta_ext(2) - d_theta_ext(1));
+        goal_speed = length_scale * (speed_ext(2) - speed_ext(1)) + speed_ext(1);
+        goal_state = [nearest_goal(1), goal_speed, nearest_goal(2), 0, 0, 0];
+        error = goal_state - curr_state;
+        goal_state(5) = centerLineTheta(:, lookahead_goal_id) -centerline_strength*dot(Cnormals(:, nearest_goal_id), [error(1);error(3)], 1); %atan2(error(3),error(1));
+        error = goal_state - curr_state;
+        sum_error = sum_error + error;
+        d_error = (error - prev_error)/0.01;
+        delta_f = kp1 * error(5) + ki1 * sum_error(5) + kd1 * d_error(5);
+        Fx = kp2 * error(2) + ki2 * sum_error(2) + kd2 * d_error(2);
+        if delta_f > 0.5
+            delta_f = 0.5;
+        end
+        if delta_f < -0.5
+            delta_f = -0.5;
+        end
+        %Solve for trajectory
+        T=(i-1)*0.01:0.01:i*0.01;
+        U = [U;[delta_f, Fx]];
+        %U(i,:) = [delta_f, Fx];
+        
+        U_sim = U(end-1:end, :);
+        if mod(i,1000) == 999
+            [Y,~]=forwardIntegrateControlInput(U,x0);
+            [~,Y1]=ode45(@(t,x)bike(t,x,T,U_sim),T,curr_state);
+            %simulation_divergence = Y(end,:)-Y1(end,:);
+        else
+            [~,Y]=ode45(@(t,x)bike(t,x,T,U_sim),T,curr_state);
+        end
+        curr_state = Y(end,:);
+        %Y_checking = [Y_checking; [curr_state, goal_state, error, simulation_divergence]];
+        prev_error = error;
+        if ((curr_state(1) > TestTrack.cline(1,end)) && (curr_state(3) > TestTrack.cline(2,end))) == 1
+            FLAG_terminate = 1;
+        end
+        %{
+        if close_to_end(curr_state, TestTrack)
+            FLAG_terminate = 1;
+            %last_input = U(end,:);
+            last_input = [0, 4000]; % was 0,4000
+            for i = 1:6000
+                U = [U;last_input];
+            end
+            sol_2 = U;
+            %break
+        end
+        %}
+    end
+    
+    sol_2 = U;
+
 end
 
 
-%% sol 2 is a vector of control inputs that will be passed to forwardIntegrateControlInput. 
-%As mentioned later in 6.2, sol 2 must have enough control inputs with time step 0.01 sec- onds to forward integrate vehicle dynamics for the next 0.5 second.
 
+function dzdt=bike(t,x,T,U)
+%constants
 Nw=2;
 f=0.01;
 Iz=2667;
@@ -25,206 +142,120 @@ Svy=0;
 m=1400;
 g=9.806;
 
+%generate input functions
+%delta_f=interp1(T,U(:,1),t,'previous','extrap');
+%F_x=interp1(T,U(:,2),t,'previous','extrap');
+delta_f = U(end,1);
+F_x = U(end,2);
 
-% (as an N × 2 vector where the first column is ? and the second column is Fx
+%slip angle functions in degrees
+a_f=rad2deg(delta_f-atan2(x(4)+a*x(6),x(2)));
+a_r=rad2deg(-atan2((x(4)-b*x(6)),x(2)));
 
-%% linearize 
+%Nonlinear Tire Dynamics
+phi_yf=(1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy));
+phi_yr=(1-Ey)*(a_r+Shy)+(Ey/By)*atan(By*(a_r+Shy));
 
-syms x1 x2 x3 x4 x5 x6 u1 u2
- 
- 
-a_f=rad2deg(u1-atan2(x4+a*x6,x2));
-a_r=rad2deg(-atan2((x4-b*x6),x2));
- 
 F_zf=b/(a+b)*m*g;
-F_yf=F_zf*Dy*sin(Cy*atan(By*((1-Ey)*(a_f+Shy)+(Ey/By)*atan(By*(a_f+Shy)))))+Svy;
- 
+F_yf=F_zf*Dy*sin(Cy*atan(By*phi_yf))+Svy;
+
 F_zr=a/(a+b)*m*g;
-F_yr=F_zr*Dy*sin(Cy*atan(By*((1-Ey)*(a_r+Shy)+(Ey/By)*atan(By*(a_r+Shy)))))+Svy;
- 
- 
-dzdx= [x2*cos(x5)-x4*sin(x5);...
-          (-f*m*g+Nw*u2-F_yf*sin(u1))/m+x4*x6;...
-          x2*sin(x5)+x4*cos(x5);...
-          (F_yf*cos(u1)+F_yr)/m-x2*x6;...
-          x6;...
-          (F_yf*a*cos(u1)-F_yr*b)/Iz];
- 
-A = jacobian(dzdx,[x1;x2;x3;x4;x5;x6]);
-B = jacobian(dzdx,[u1;u2]);
- 
-%% Decision Variables 
-% ith # of state : 6, input : 2 ;
-% horizon of 0.5 s & 0.01 s = 50 timestep
-Ndec = 6*51 + 2*5;
-npred = 50;
-xsize = 6*51;
-zsize = Ndec;
+F_yr=F_zr*Dy*sin(Cy*atan(By*phi_yr))+Svy;
 
-% Staates : z  &  Input : u 
-%z = zeros(6*51 + 2*5, 1 );
+F_total=sqrt((Nw*F_x)^2+(F_yr^2));
+F_max=0.7*m*g;
 
-
-
-% State Constraints 
-
-[leftlimit, rightlimit] = bound(npred,states,leftbound, rightbound);
-state_lb = -inf(Ndec,1);
-state_ub = inf(Ndec,1);
-
-for i = 1 : npred
-    state_lb(6* (i-1) +1) =  min(leftlimit(1,i), rightlimit(1,i));
-    state_lb(6* (i-1) +3) =  min(leftlimit(2,i), rightlimit(2,i));
+if F_total>F_max
     
-    state_ub(6* (i-1) +1) = max(leftlimit(1,i), rightlimit(1,i));
-    state_ub(6* (i-1) +3) = max(leftlimit(2,i), rightlimit(2,i));
+    F_x=F_max/F_total*F_x;
+  
+    F_yr=F_max/F_total*F_yr;
+end
+
+%vehicle dynamics
+dzdt= [x(2)*cos(x(5))-x(4)*sin(x(5));...
+          (-f*m*g+Nw*F_x-F_yf*sin(delta_f))/m+x(4)*x(6);...
+          x(2)*sin(x(5))+x(4)*cos(x(5));...
+          (F_yf*cos(delta_f)+F_yr)/m-x(2)*x(6);...
+          x(6);...
+          (F_yf*a*cos(delta_f)-F_yr*b)/Iz];
 end
 
 
+function is_close = close_to_end(curr_state,TestTrack)
+    x = curr_state(1);
+    y = curr_state(3);
+    last_point = TestTrack.cline(:,end);
+    dist = norm(last_point-[x;y]);
+    is_close = ( dist < 10 ); % was 3
+end
 
-
-% Input constraints 
-    Lb = -inf(Ndec,1);
-    Ub = inf(Ndec,1);
-    j=0;
-    for k = 1: npred
-        Lb(xsize+2*(k-1) + 1) = 0 - U(1,i+j);
-        Lb(xsize+2*(k-1) + 2) = -0.5 - U(2,i+j);
- 
-        Ub(xsize+2*(k-1) + 1) = 1 -U(1,i+j);
-        Ub(xsize+2*(k-1) + 2) = 0.5 -U(2,i+j);
-        j = j +1;
+function closest_obstacle_center = get_closest_obstacle(curr_state, Xobs_seen)
+    num_cells = numel(Xobs_seen);
+    if num_cells == 0
+        closest_obstacle_center = [0;0];
+        %disp("NO CELLS!");
+    else
+        min_dist = 999999;
+            for i = 1:num_cells
+                obstacle = Xobs_seen{i};       
+                if curr_state(1) > 900
+                   if curr_state(1) < obstacle(1) % compare x when curr_state(1) > 900
+                        obstacle_center = mean(obstacle)';
+                        obstacle_dist = norm(obstacle_center - [curr_state(1); curr_state(3)]);
+                        if obstacle_dist < min_dist
+                            min_dist = obstacle_dist;
+                            closest_obstacle_center = obstacle_center;
+                        end
+                   else
+                   closest_obstacle_center = [0;0];
+                   end
+                   
+                else
+                    if curr_state(2) < obstacle(2) % compare y when curr_state(1) < 900
+                        obstacle_center = mean(obstacle)';
+                        obstacle_dist = norm(obstacle_center - [curr_state(1); curr_state(3)]);
+                        if obstacle_dist < min_dist
+                            min_dist = obstacle_dist;
+                            closest_obstacle_center = obstacle_center;
+                        end
+                    else
+                    closest_obstacle_center = [0;0];
+                    end
+                end
+            end
+        
+        hold on;
+        scatter(closest_obstacle_center(1), closest_obstacle_center(2));
     end
-
-
-%% quadatic penalty
-% !! have to call the result of part 1 for calling Y_ref / U_ref !! 
-    STATE_tilde = urr_state - Y_ref;
-    [Aeq,beq] = eq_cons(npred, xsize, zsize, A,B, STATE_tilde);
-    [Lb,Ub] = bound_cons(U_ref);
-    
-    
-    sol = quadprog(H,zeros(53,1),[],[],Aeq,beq,Lb,Ub);
-
-
-    %extract control input from solution   
-    %forward integrate using nonlinear model, STATE(i), the extracted control input and ode45
-    uin=sol(xsize+1 : xsize+2,1) + U_ref;
-
-
-
-%% FLAG terminate is a binary flag set by the function to indicate when to stop the simulation. Setting this flag to 1 at a planning iteration indicates that, after integrating forward vehicle dynamics using the control inputs from sol 2 for 0.5 second, the vehicle would have reached the goal. If this flag is set to 0, then it implies that the car would not reach the goal after forward integrating for 0.5 second. Note that, in the event that FLAG terminate is never set to 1, the simulation would stop after 20 minutes so that we can process all the teams files in a timely manner.
-
-
-
-
-
-function [Aeq,Beq] = eq_cons(npred, xsize, zsize, A,B, curr_state)
-
-Aeq=zeros(xsize,zsize);
-Beq=zeros(xsize,1);
-
-for i = 1 : npred
-    Am = A(i);
-    Bm = B(i);
-    
-    Aeq(6*(i)+1 :6*(i)+6 , 6*(i-1)+1:6*(i-1)+6) = Am ;
-    Aeq(6*(i)+1: 6*(i)+6, 6*(i)+1 :6*(i)+6 ) = Aeq(6*(i)+1: 6*(i)+6, 6*(i)+1 :6*(i)+6) -eye(6);  
-    
-    Aeq(6*(i)+1:6*(i)+6 ,xsize+2*(i-1)+1:xsize+2*(i-1)+2) = Bm;
 end
 
-Aeq(1:6,1:6) = eye(6);
-Beq(1:6) = reshape(curr_state,6,1);
-
-end
-
-
-
-function[leftlimit, rightlimit] = bound(npred,states,leftbound, rightbound)
-    
-    % Output : leftlimit and the right limit of the road for the current
-    % location
-     
-    xypos = zeros(npred+1,2);
-    leftlimit = -inf(size(states))
-    rightlimit = inf(size(states))
-    
-    
-    for i = 1 : npred+1
-        xypos(i,1) = states(6 * (i-1) +1);
-        xypos(i,2) = states(6 * (i-1) +3);
-        Idleft_1 = knnsearch(leftBound', xypos(i,:));
-        Idright_1 = knnsearch(rightBound', xypos(i,:));
+function centerLine = get_lane_from_obstacle_center( closest_obstacle_center, TestTrack)
+    if closest_obstacle_center == [0;0]
+        disp("Center")
+        centerLine = TestTrack.cline;
+    else
         
-        % find a left and right pair of points to do a linear interpolation
-        % in order to get the exact leftbound and rightbound of a given
-        % position. Linear interpolation is done by using a "dot product".
+        n_elements = size(TestTrack.cline, 2);
+        projectors = (TestTrack.cline(:,2:n_elements) - TestTrack.cline(:,1:n_elements-1));
+        projectors = projectors ./ vecnorm(projectors);
+        Cnormals = [0, 1; -1, 0] * projectors;
+        Cnormals(:,n_elements) = Cnormals(:,n_elements - 1);
         
-        if dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1 +1)-leftbound(:, Idleft_1)) >= 0 
-            leftlimit = leftbound(:,Idleft_1) + dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1 +1)-leftbound(:, Idleft_1));
+        % needs to take in centerLine and Cnormals
+        % pos_obs needs to be row for knn to work
+        cline_idx = knnsearch(TestTrack.cline', closest_obstacle_center');
+        obs_vec = TestTrack.cline(:,cline_idx) - closest_obstacle_center;
+        norm_vec = Cnormals(:,cline_idx);
+        sign_check = dot(obs_vec, norm_vec);
+        % positive = RHS
+        % negative = LHS
+        if sign_check >= 0
+            disp("Right")
+            centerLine = (TestTrack.br + TestTrack.cline)./2;
         else
-            leftlimit = leftbound(:,Idleft_1) + dot(xypos(i,:) - leftbound(:,Idleft_1),leftbound(:, Idleft_1-1)-leftbound(:, Idleft_1));
+            disp("Left")
+            centerLine = (TestTrack.bl + TestTrack.cline)./2;
         end
-        
-        if dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 +1)-xypos(i,:)) >= 0 
-            rightlimit = dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 +1)-xypos(i,:));
-        else
-            rightlimit = dot(xypos(i,:) - rightbound(:,Idright_1),rightbound(:,Idright_1 -1)-xypos(i,:));
-        end
-    end 
- 
-end
-
-
-%function restricting ith x and y with 1st order function
-% for a and b 
-
-function[a, b] = first_ineq(zsize,npred,states,leftbound,rightbound)
-   
-    a = zeros(2*(npred+1), zsize);
-    b = zeros(2*(npred+1),1);
-
-     
-     for i = 1 : npred+1
-        xpos = states(6 * (i-1) +1);
-        ypos = states(6 * (i-1) +3);
-        Idleft_1 = knnsearch(leftBound', [xpos,ypos]');
-        Idright_1 = knnsearch(rightBound',[xpos,ypos]');
-        
-        left_1 = leftbound(:,Idleft_1);
-        right_1 = rightbound(:,Idright_1);
-        
-        if dot([xpos,ypos]' - left_1,leftbound(:, Idleft_1 +1)-left_1) >= 0 
-            left_2 = left_1  + dot([xpos,ypos]' - lleft_1 ,leftbound(:, Idleft_1 +1)-left_1 );
-        else
-            left_2 = left_1  + dot([xpos,ypos]' - left_1 ,leftbound(:, Idleft_1-1)-left_1 );
-        end
-        
-        if dot([xpos,ypos]' - left_1,rightbound(:,Idright_1 +1)-left_1) >= 0 
-            right_2 = right_1+ dot([xpos,ypos]' - right_1,rightbound(:,Idright_1 +1)-right_1);
-        else
-            right_2 = right_1+ dot([xpos,ypos]' - right_1,rightbound(:,Idright_1 -1)-right_1);
-        end
-        
-        left_slope = (left_1(1) - left_2(1)) / (left_1(2) - left_2(2));
-        right_slope = (right_1(1) - right_2(1)) / (right_1(2) - right_2(2));
-        
-        a(2*(i-1) +1, 6 * (i-1) +1) = - left_slope;
-        a(2*(i-1) +1, 6 * (i-1) +3) = 1;
-        a(2*(i-1) +2, 6 * (i-1) +1) = - left_slope;
-        a(2*(i-1) +1, 6 * (i-1) +3) = -1;
-        
-        b(2*(i-1) +1) = left_slope * (-left_1(1)) + left_1(2);
-        b(2*(i-1) +2) = right_slope * (left_1(1)) - left_1(2);
-        
-     end       
- end
-     
-
-% avoiding obstacles by elongating(adding) some rows a and b constraints % 
-
-function avoiding_obstacles(Xobs_seen)
-    obs_num = length(Xobs_seen)
-
+    end
 end
--- test_entire_trajectory.m
+++ test_entire_trajectory.m
@@ -1,61 +1,37 @@
-clear
+
 load TestTrack.mat
+load ROB535_ControlsProject_part1_Team1.mat
 
-% Interpolate Test Track?
-num_interpl = 5;
-cline_interp = interp_cline(TestTrack.cline, num_interpl);
-plot(TestTrack.cline(1,:), TestTrack.cline(2,:) )
+plot(TestTrack.cline(1,:), TestTrack.cline(2,:),'r' )
 hold on
-plot(cline_interp(1,:), cline_interp(2,:) )
+plot(TestTrack.br(1,:), TestTrack.br(2,:),'r' )
+hold on
+plot(TestTrack.bl(1,:), TestTrack.bl(2,:),'r' )
+hold on
+%Xobs = generateRandomObstacles(9 + randi(16),TestTrack);
 
-start_pos =[287,5,-176,0,2,0]';
+[Y,U,Xobs, t_total,t_update] = forwardIntegrate();
+info = getTrajectoryInfo(Y,U,Xobs,TestTrack)
+plot(Y(:,1), Y(:,3),'k' )
+%[Uref, Yref,T] = create_reference();  % when there are no obstacles 
+%[Yref,U,t_total,t_update] = forwardIntegrate(); %when there are obstacles
+%plot(Yref(:,1), Yref(:,3),'k')
 
-Y(1,:) = start_pos;
+for i = 1 : length(Xobs)
+Obs= cell2mat(Xobs(i));
 
-% Window size when segmenting track
-N = 100;
-
-% Uncomment this section to check part 1:
-sol1 = forwardIntegrateControlInput(ROB535ControlsProjectpart1input)
-
-
-%for i = 1:N:size(cline_interp,2)-N+1
-for i = 1
-    % Start pos
-    curr_state = Y(i,:);
-    
-    % Estimate the test_track
-    %track_section = TestTrack.cline(:,i:i+5);
-    track_section = cline_interp(:,i:i+N-1);
-    
-    %Generate N inputs based on current position
-    U = getControlInput(curr_state, track_section);
-    
-    %Simulate forwards to get next state from U
-    [Y_temp,T]=forwardIntegrateControlInput(U,curr_state);
-    
-    
-    Y(i+1:i+N,:) = Y_temp;
-    
-%     subplot(2,1,1)
-%     plot(track_section(1,:),track_section(2,:))
-%     
-%     subplot(2,1,2)
-%     plot( Y(1,:), Y(3,:) )
-end
-
-function U = getControlInput(curr_state, track_section)
-    U = zeros(size(track_section,2));
+x = [Obs(1,1), Obs(2,1), Obs(3,1), Obs(4,1), Obs(1,1)];
+y = [Obs(1,2), Obs(2,2), Obs(3,2), Obs(4,2), Obs(1,2)];
+plot(x, y, 'b-', 'LineWidth', 0.5);
+hold on
 end
 
 
-function cline_interp = interp_cline(cline, num_interpl)
-    num_points = size(cline,2);
-    
-    for i = 1:num_points
-        x = cline(1,i);
-        y = cline(2,i);
-    end
-    
-    cline_interp = cline;
+
+function [Uref, Yref,T] = create_reference()
+    load ROB535_ControlsProject_part1_Team1.mat
+
+    x0 =[287,5,-176,0,2,0];
+    Uref = ROB535_ControlsProject_part1_input;
+   [Yref,T]=forwardIntegrateControlInput(ROB535_ControlsProject_part1_input,x0);
 end
